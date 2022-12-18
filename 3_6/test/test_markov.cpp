902824c38928a59be1cffbc54e0e4019fdc37255
Fujisaki <whentheycry0708fi@gmail.com>
2022-12-18 16:48:09 +0900

    add problem 3_2

--- 3_6/test/test_markov.cpp
+++ 3_6/test/test_markov.cpp
@@ -1,4 +1,4 @@
-#include "../markov.h"
+#include "../markov.cpp"
 #include <map>
 #include <algorithm>
 #include <iterator>
@@ -21,7 +21,11 @@ TEST(MARKOV_TEST, add){
     EXPECT_EQ(prefix.size(), NUMBER_OF_PREFIX);
 
     for(uint32_t i = 0; i < NUMBER_OF_PREFIX; ++i){
+#ifdef problem_3_2
+        EXPECT_EQ(prefix.front(), hash(NONWORD));
+#else
         EXPECT_EQ(prefix.front(), NONWORD);
+#endif
         prefix.pop_front();
     }
 
@@ -35,9 +39,17 @@ TEST(MARKOV_TEST, build){
     std::ifstream is0("test_case0.txt");
 
     build(prefix, is0);
+#ifdef problem_3_2
+    EXPECT_EQ(prefix.front(), hash("pitch"));
+#else
     EXPECT_EQ(prefix.front(), "pitch");
+#endif
     prefix.pop_front();
+#ifdef problem_3_2
+    EXPECT_EQ(prefix.front(), hash("sine"));
+#else
     EXPECT_EQ(prefix.front(), "sine");
+#endif
     prefix.pop_front();
 
     EXPECT_EQ(prefix.size(), 0);
@@ -58,5 +70,13 @@ TEST(MARKOV_TEST, generate){
     generate(nwords);
     
     EXPECT_EQ(prefix.size(), NUMBER_OF_PREFIX);
+}
+
+TEST(MARKOV_TEST, define){
+#ifdef problem_3_2
+    std::cout << "ver 3_2" << std::endl;
+#else
+    std::cout << "ver default" << std::endl;
+#endif
 
 }
